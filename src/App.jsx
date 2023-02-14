b1b3f52a4dec0899344b0734f17188f5d33835ae
George <ares@ares.lan>
2023-02-15 08:27:38 +1100

    moving from CRA -> Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import DropDownMenu from "./components/DropDownMenu";
-import Visualiser from "./components/Visualiser";
+import DropDownMenu from './components/DropDownMenu';
+import Visualiser from './components/Visualiser';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">Visualiser</header>
       <div className="App-background">
-        <Visualiser></Visualiser>
-        <DropDownMenu></DropDownMenu>
+      <DropDownMenu></DropDownMenu>
+      <Visualiser></Visualiser>
       </div>
     </div>
-  );
+  )
 }
